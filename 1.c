df32884d52181bde1c9e5228a6026355978baf33
Ubuntu <kokitodaito>
2022-07-28 18:15:58 +0800

    First Commit

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[1000];
+    int a=0;
+    
+    while(scanf("%s",str)!=EOF)
+    {}
+    a=strlen(str);
+    printf("%d\n",a);
+    return 0;
+}
